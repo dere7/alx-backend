3d117029a7ca1156d9b7a4a14abc0dc16728ddbd
Dereje Desta <ddesta777@gmail.com>
2022-07-19 21:45:11 +0300

    worked on task 5 to 8

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""configures flask-babel"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from typing import Optional, Mapping
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """conifguration for app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> Optional[str]:
+    """selects a languages translation to be used for each request"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return request.args['locale']
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Optional[Mapping]:
+    """returns user dictionary"""
+    user_id = request.args.get('login_as')
+    if user_id and int(user_id) in users:
+        return users.get(int(user_id))
+    return None
+
+
+@app.before_request
+def before_request():
+    """gets user before each request"""
+    user = get_user()
+    if user:
+        g.user = user
+
+
+@app.route('/')
+def home() -> str:
+    """home displays 'hello world'"""
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ 0x02-i18n/8-app.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+"""configures flask-babel"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+import pytz
+from typing import Optional, Mapping
+from flask_babel import format_datetime
+from datetime import datetime
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """conifguration for app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> Optional[str]:
+    """selects a languages translation to be used for each request"""
+
+    # locale from url parameters
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return request.args['locale']
+
+    # locale from user setting
+    user = getattr(g, 'user', None)
+    if user and user.get('locale') in app.config['LANGUAGES']:
+        return user.get('locale')
+
+    # locale from request header
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@babel.timezoneselector
+def get_timezone() -> Optional[str]:
+    """gets a timezone from query or user setting"""
+    try:
+        # get timezone from url parameter
+        timezone = request.args.get('timezone')
+        if timezone and pytz.timezone(timezone):
+            return timezone
+
+        # find timezone from user settings
+        user = getattr(g, 'user', None)
+        if user and pytz.timezone(user.get('timezone')):
+            return user.get('timezone')
+    except pytz.exceptions.UnknownTimeZoneError:
+        pass
+
+    # default timezone
+    return app.config['BABEL_DEFAULT_TIMEZONE']
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Optional[Mapping]:
+    """returns user dictionary"""
+    user_id = request.args.get('login_as')
+    if user_id and int(user_id) in users:
+        return users.get(int(user_id))
+    return None
+
+
+@app.before_request
+def before_request():
+    """gets user before each request"""
+    user = get_user()
+    if user:
+        g.user = user
+
+
+@app.route('/')
+def home() -> str:
+    """home displays 'hello world'"""
+    current_time = format_datetime(datetime.now(), 'LLL d, Y, H:mm:ss a')
+    return render_template('8-index.html', current_time=current_time)
+
+
+if __name__ == '__main__':
+    app.run()
