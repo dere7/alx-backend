bf78e47111f3865360ce80d2b62c3140038616df
Dereje Desta <ddesta777@gmail.com>
2022-07-19 18:32:28 +0300

    Initial commit for 0x02-i18n

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""configures flask-babel"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def home() -> str:
+    """home renders html with 'hello world' header"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""configures flask-babel"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+from typing import Optional
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> Optional[str]:
+    """selects a languages translation to be used each request"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def home() -> str:
+    """home displays 'hello world'"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
