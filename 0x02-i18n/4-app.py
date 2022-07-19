b6ba4ffa9f2ef39f3130548822860bc841b3be8b
Dereje Desta <ddesta777@gmail.com>
2022-07-19 19:46:10 +0300

    worked up to 4th task

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -20,6 +20,9 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale() -> Optional[str]:
     """selects a languages translation to be used for each request"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return request.args['locale']
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
